21071bbe49328912cc9803d58980c173d8920e6c
jasper200207 <jasper12394@gmail.com>
2022-05-26 14:31:47 +0000

    append_left insert_left delete_node delete get_node

--- libs/linkedlist.c
+++ libs/linkedlist.c
@@ -41,35 +41,58 @@ void print_file(FILE* stream){
 void clear() {
     Node* curr = _tail->prev;
     whlie(curr != _head) {
-        curr = delete_node(curr);
+        curr = curr->prev;
+        delete_node(curr->next);
     }
     printf("LinkedList is cleared!");
 }
 
-Node* append_left(size_t n, char new_data[n]){}
+Node* append_left(size_t n, char new_data[n]){
+    Node* curr = _head;
+    int i;
+    for(i=0;i<n;i++) {
+        Node new = {new_data[i],curr,curr->next};
+        curr->next->prev = &new;
+        curr->next = new;
+        curr = new;
+    }
+}
 
-Node* insert_after(Node* cur_node, Node* new_node){}
+Node* insert_after(Node* cur_node, Node* new_node){
+    cur_node->next->prev = new_node;
+    new_node->next = cur_node->next;
+    cur_node->next = new_node;
+    new_node->prev = cur_node;
+}
 
 Node* append(size_t n, char new_date[n]){}
 
 Node* delete_node(Node* cur_node){
-    Node* _temp_node = cur_node->next->prev = cur_node->prev;
+    cur_node->next->prev = cur_node->prev;
     cur_node->prev->next = cur_node->next;
-    free(cur_node);
-    return _temp_node;
+    return cur_node
 }
 
 Node* delete(char* data){
     Node* curr = _head->next;
     while(curr != _tail) {
         if(strcmp(curr->data,data) == 0) {
-            delete_node(curr);
-            break;
+            return delete_node(curr);
         }
     }
+    return NULL;
 }
 
-Node* get_node(size_t index){}
+Node* get_node(size_t index){
+    size_t now_index = 0;
+    Node* curr = head->next;
+    while(now_index < index) {
+        if(curr == _tail) return NULL;
+        curr = curr->next;
+        now_index++;
+    }
+    return curr;
+}
 
 Node* first();
 
