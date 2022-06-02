53ca0a5c11e7e22a125f7bf6889ee1e9196e5460
강준우 <0119299@gmail.com>
2022-06-02 16:18:01 +0900

    read_file 함수 구현 : 온전함

---
+++ libs/textfilewriter.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "textfilewriter_2.h"
+#define MAX_TITLE_SIZE 50
+
+void create_music_titles(FILE* stream) { // 한줄씩 파일의 내용을 읽는 것 까지 구현
+	char* music_titles;
+	char buffer[1000];
+
+	while (!feof(stream)) {
+		music_titles = fgets(buffer, sizeof(buffer), stream);
+		printf("%s", music_titles);
+	}
+}
+void read_file(char* file_name) { // 파일의 이름은 test.txt로 고정하고 구현하였음 -> 이는 Player.c에서 적음
+	// 파일 포인터에 text를 배정하고 create_m_t()함수의 인자로 넘겨줌.
+	FILE* fp = NULL;
+	if (fopen_s(&fp, file_name, "r") == 0) {
+		printf("파일을 열었습니다.\n");
+		create_music_titles(fp);
+	}
+	else {
+		printf("파일을 열지 못했습니다.\n");
+	}
+
+	fclose(fp);
+}
